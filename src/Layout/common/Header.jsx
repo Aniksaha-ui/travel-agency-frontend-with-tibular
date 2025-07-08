1250e1cd379e99a112aef50d2cd4f7ac162624d8
Aniksaha-ssl <anik.saha@sslwireless.com>
2025-07-08 11:31:49 +0600

    header changed

--- src/Layout/common/Header.jsx
+++ src/Layout/common/Header.jsx
@@ -398,7 +398,224 @@ const Header = () => {
                     <span className="nav-link-title">Home</span>
                   </a>
                 </li>
-                <li className="nav-item active dropdown">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/admin/users">
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
+                      {/* Download SVG icon from http://tabler-icons.io/i/home */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx="9" cy="7" r="4" />
+                        <path d="M17 11v-1a4 4 0 0 0 -3 -3.85" />
+                        <path d="M3 21v-2a4 4 0 0 1 4 -4h4" />
+                        <path d="M16 19h6" />
+                        <path d="M19 16v6" />
+                      </svg>
+                    </span>
+                    <span className="nav-link-title">User Management</span>
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link className="nav-link" to="/admin/routes">
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
+                      {/* Download SVG icon from http://tabler-icons.io/i/home */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx="6" cy="6" r="2" />
+                        <circle cx="18" cy="6" r="2" />
+                        <circle cx="6" cy="18" r="2" />
+                        <path d="M6 8v8a2 2 0 0 0 2 2h8" />
+                        <polyline points="14 14 18 10 18 8" />
+                      </svg>
+                    </span>
+                    <span className="nav-link-title">Route Management</span>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/admin/vehicles">
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
+                      {/* Download SVG icon from http://tabler-icons.io/i/home */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <rect x="3" y="11" width="18" height="6" rx="2" />
+                        <path d="M5 11l1.5 -4.5h11l1.5 4.5" />
+                        <circle cx="7" cy="17" r="2" />
+                        <circle cx="17" cy="17" r="2" />
+                      </svg>
+                    </span>
+                    <span className="nav-link-title">Vehicle Management</span>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/admin/seat">
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
+                      {/* Download SVG icon from http://tabler-icons.io/i/home */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <rect x="6" y="5" width="12" height="8" rx="2" />
+                        <path d="M6 13v3a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2v-3" />
+                        <path d="M6 19h12" />
+                      </svg>
+                    </span>
+                    <span className="nav-link-title">Seat Management</span>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/admin/trips">
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
+                      {/* Download SVG icon from http://tabler-icons.io/i/home */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx="12" cy="12" r="9" />
+                        <path d="M12 21v-2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v2" />
+                        <path d="M12 3a12 12 0 0 1 12 12a12 12 0 0 1 -12 12a12 12 0 0 1 -12 -12a12 12 0 0 1 12 -12" />
+                      </svg>
+                    </span>
+                    <span className="nav-link-title">Trip Management</span>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/admin/trips">
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
+                      {/* Download SVG icon from http://tabler-icons.io/i/home */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx="9" cy="7" r="4" />
+                        <path d="M17 11v-1a4 4 0 0 0 -3 -3.85" />
+                        <path d="M3 21v-2a4 4 0 0 1 4 -4h4" />
+                        <path d="M16 19h6" />
+                        <path d="M19 16v6" />
+                      </svg>
+                    </span>
+                    <span className="nav-link-title">Guide Management</span>
+                  </Link>
+                </li>
+              </ul>
+
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/admin/bookings">
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
+                      {/* Download SVG icon from http://tabler-icons.io/i/home */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M12 3l2 4h-4l2 -4z" />
+                        <path d="M12 21v-10" />
+                        <path d="M9 14h6" />
+                        <path d="M5 19l4 -4" />
+                        <path d="M19 19l-4 -4" />
+                      </svg>
+                    </span>
+                    <span className="nav-link-title">Bookings</span>
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link className="nav-link" to="/admin/refunds">
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
+                      {/* Download SVG icon from http://tabler-icons.io/i/home */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M12 3l2 4h-4l2 -4z" />
+                        <path d="M12 21v-10" />
+                        <path d="M9 14h6" />
+                        <path d="M5 19l4 -4" />
+                        <path d="M19 19l-4 -4" />
+                      </svg>
+                    </span>
+                    <span className="nav-link-title">Refunds</span>
+                  </Link>
+                </li>
+
+                {/* <li className="nav-item active dropdown">
                   <a
                     className="nav-link dropdown-toggle"
                     href="#navbar-base"
@@ -408,7 +625,6 @@ const Header = () => {
                     aria-expanded="false"
                   >
                     <span className="nav-link-icon d-md-none d-lg-inline-block">
-                      {/* Download SVG icon from http://tabler-icons.io/i/package */}
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="icon"
@@ -467,7 +683,7 @@ const Header = () => {
                       </div>
                     </div>
                   </div>
-                </li>
+                </li> */}
 
                 <li className="nav-item dropdown">
                   <a
@@ -532,38 +748,6 @@ const Header = () => {
                   </div>
                 </li>
               </ul>
-              <div className="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
-                <form action="./" method="get" autoComplete="off" noValidate>
-                  <div className="input-icon">
-                    <span className="input-icon-addon">
-                      {/* Download SVG icon from http://tabler-icons.io/i/search */}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="icon"
-                        width={24}
-                        height={24}
-                        viewBox="0 0 24 24"
-                        strokeWidth={2}
-                        stroke="currentColor"
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-                        <path d="M21 21l-6 -6" />
-                      </svg>
-                    </span>
-                    <input
-                      type="text"
-                      defaultValue
-                      className="form-control"
-                      placeholder="Search…"
-                      aria-label="Search in website"
-                    />
-                  </div>
-                </form>
-              </div>
             </div>
           </div>
         </div>
