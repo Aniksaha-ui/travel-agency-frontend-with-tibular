5703520e8d81e861f5a76ce220475d8978664c13
Aniksaha-ssl <anik.saha@sslwireless.com>
2025-09-14 13:14:08 +0600

    icon spearation

---
+++ src/Utils/Constants/svg.jsx
@@ -0,0 +1,63 @@
+export const UserManagementIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <circle cx="9" cy="7" r="4" />
+    <path d="M17 11v-1a4 4 0 0 0 -3 -3.85" />
+    <path d="M3 21v-2a4 4 0 0 1 4 -4h4" />
+    <path d="M16 19h6" />
+    <path d="M19 16v6" />
+  </svg>
+);
+
+export const DashboardIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+    <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+    <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+  </svg>
+);
+
+export const RouteManagementIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <circle cx="6" cy="6" r="2" />
+    <circle cx="18" cy="6" r="2" />
+    <circle cx="6" cy="18" r="2" />
+    <path d="M6 8v8a2 2 0 0 0 2 2h8" />
+    <polyline points="14 14 18 10 18 8" />
+  </svg>
+);
