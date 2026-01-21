790c15c02fceddae7ff6ea323284c45173c54584
Aniksaha-ssl <anik.saha@sslwireless.com>
2026-01-21 17:15:50 +0600

    menu js file

---
+++ src/Utils/Constants/menus.js
@@ -0,0 +1,55 @@
+export const DASHBOARD_MENU = "/admin/dashboard";
+export const USER_MANAGEMENT_MENU = "/admin/users";
+export const ROUTE_MANAGEMENT_MENU = "/admin/routes";
+export const ADD_ROUTE_MENU = "/admin/routes/add";
+export const VEHICLE_MANAGEMENT_MENU = "/admin/vehicles";
+export const ADD_VEHICLE_MENU = "/admin/vehicles/add";
+export const BOOK_VEHICLE_FOR_TRIP_MENU = "admin/vehicles/bookfortrip/:id";
+export const SEAT_MANAGEMENT_MENU = "/admin/seat";
+export const ADD_SEAT_MENU = "/admin/seat/add";
+
+export const VEHICLE_SEAT_LAYOUT_MENU = "admin/vehicleSeatLayout/:id";
+export const TRIPS_MENU = "admin/trips";
+export const TRIP_DETAILS_MENU = "admin/trips/:tourId";
+export const ADD_TRIPS_MENU = "admin/trips/add";
+export const TRIP_UPDATE_MENU = "/admin/trips/update/:id";
+
+export const PACKAGES_MENU = "/admin/packages";
+export const PACKAGE_DETAILS_MENU = "/admin/packages/:id";
+export const ADD_PACKAGE_MENU = "/admin/packages/add";
+export const UPDATE_PACKAGE_MENU = "/admin/packages/update/:id";
+
+export const BOOKING_MENU = "admin/bookings";
+export const BOOKING_INVOICE = "admin/bookinginvoice/:id";
+
+export const REFUNDS_MENU = "/admin/refunds";
+export const GUIDE_MENU = "/admin/guide";
+export const ADD_GUIDE_MENU = "/admin/guide/add";
+export const UPDATE_GUIDE_MENU = "/admin/guide/update/:id";
+
+export const HOTEL_MENU = "/admin/hotel";
+export const ADD_HOTEL_MENU = "/admin/hotel/add";
+export const UPDATE_HOTEL_MENU = "/admin/hotel/update/:id";
+export const HOTEL_CHECKIN_MENU = "/admin/hotel/checkin";
+export const VEHICLE_WISE_SEAT_REPORT_MENU = "/admin/vehiclewiseseatreport";
+
+export const ACCOUNT_BALANCE_REPORT_MENU = "/admin/account/balance";
+
+export const VEHICLE_TRACKING_REPORT_MENU = "/admin/vehicletrackingreport";
+export const TRIP_PERFORMANCE = "admin/tripPerformance";
+export const PACKAGE_PERFORMANCE = "admin/packagePerformance";
+export const CUSTOMER_VALUE_REPORT = "admin/customerValueReport";
+
+export const MONTHLY_RUNNING_BALANCE = "admin/monthRunningBalance";
+export const FINANCIAL_REPORT = "admin/financialReport";
+export const TICKET_MANAGEMENT = "admin/tickets";
+export const TRANSACTIONS_MANAGEMENT_MENU = "/admin/transactions";
+export const QUERY_MONITORING = "admin/monitoring";
+
+export const GUIDE_ASSIGN_PACKAGE = "guide/myAssignPackages";
+export const GUIDE_PACKAGE_COSTING = "guide/my-packageCosting/:id";
+export const GUIDE_ADD_COSTING =
+  "guide/my-packageCosting/addCosting/:packageId";
+export const GUIDE_COSTING_DETAILS =
+  "guide/my-packageCosting/addCosting/:packageId/:costId";
+export const GUIDE_FEEDBACKS = "guide/my-feedback/:id";
