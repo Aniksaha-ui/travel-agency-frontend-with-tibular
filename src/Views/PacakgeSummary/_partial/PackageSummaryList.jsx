0af5fdf23d220156686eabf36a2a2b90738c2e48
Aniksaha-ssl <anik.saha@sslwireless.com>
2026-01-27 13:34:29 +0600

    trip package booking

--- src/Views/PacakgeSummary/_partial/PackageSummaryList.jsx
+++ src/Views/PacakgeSummary/_partial/PackageSummaryList.jsx
@@ -14,18 +14,28 @@ export const PackageSummaryList = ({ packageSummaryInfo }) => {
           </tr>
         </thead>
         <tbody>
-          {packageSummaryInfo?.map((pack) => (
-            <tr
-              key={pack.id}
-              className=""
-            >
-              <td>{pack.package_id}</td>
-              <td>{pack.package_name}</td>
-              <td>{pack.number_of_bookings}</td>
-              <td>{pack.total_adult}</td>
-              <td>{pack.total_child}</td>
-            </tr>
-          ))}
+          {packageSummaryInfo?.map((pack) => {
+            const totalAdult = Number(pack.total_adult) || 0;
+            const totalChild = Number(pack.total_child) || 0;
+            const totalPeople = totalAdult + totalChild;
+
+            const adultPercent = totalPeople
+              ? Math.round((totalAdult / totalPeople) * 100)
+              : 0;
+            const childPercent = totalPeople
+              ? 100 - adultPercent
+              : 0;
+
+            return (
+              <tr key={pack.package_id}>
+                <td>{pack.package_id}</td>
+                <td>{pack.package_name}</td>
+                <td>{pack.number_of_bookings}</td>
+                <td>{totalAdult} ({adultPercent}%)</td>
+                <td>{totalChild} ({childPercent}%)</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </ResponsiveTable>
